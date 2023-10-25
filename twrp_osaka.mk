8f2eb2fc69e9686b2a3f53d4cf089955fcfc9941
tullyp <54035666+tullyp@users.noreply.github.com>
2023-10-25 16:56:46 -0400

    Update and rename twrp_denver.mk to twrp_osaka.mk

--- twrp_osaka.mk
+++ twrp_osaka.mk
@@ -13,14 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from denver device
-$(call inherit-product, device/motorola/denver/device.mk)
+$(call inherit-product, device/motorola/osaka/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := denver
-PRODUCT_NAME := twrp_denver
+PRODUCT_DEVICE := osaka
+PRODUCT_NAME := twrp_osaka
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g stylus 5G
 PRODUCT_MANUFACTURER := motorola
